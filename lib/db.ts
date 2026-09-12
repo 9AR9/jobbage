edb5834af9848f92f92ecb8aef2b1db68a43aba8
Allen Rothschild <26131176+9AR9@users.noreply.github.com>
2026-09-11 18:28:33 -0700

    chore: add mongodb and better auth

---
+++ lib/db.ts
@@ -0,0 +1,55 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if(!MONGODB_URI) {
+    throw new Error("Please define the MONGODB_URI environment variable inside .env");
+}
+
+interface MongooseCache {
+    connection: typeof mongoose | null;
+    promise: Promise<typeof mongoose> | null;
+}
+
+declare global {
+    var mongoose: MongooseCache | undefined;
+}
+
+let cached: MongooseCache = global.mongoose || { connection: null, promise: null };
+
+if (!global.mongoose) {
+    global.mongoose = cached;
+}
+
+async function connectDB() {
+  if (!MONGODB_URI) {
+    throw new Error(
+      "Please define the MONGODB_URI environment variable inside .env"
+    );
+  }
+
+  if (cached.connection) {
+    return cached.connection;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+
+    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+      return mongoose;
+    });
+  }
+
+  try {
+    cached.connection = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+
+  return cached.connection;
+}
+
+export default connectDB;
